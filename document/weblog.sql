bc3f8fcf5dc2705f4df9e8d8e5538782c236a3e4
cc <2967861744@qq.com>
2023-08-29 10:26:36 +0800

    全栈实战个人博客系统1.3版本,解决前端打包路由错误，修复了一些bug

--- document/weblog.sql
+++ document/weblog.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80032
  File Encoding         : 65001
 
- Date: 17/08/2023 19:41:21
+ Date: 29/08/2023 10:26:21
 */
 
 SET NAMES utf8mb4;
@@ -44,12 +44,12 @@ CREATE TABLE `article`  (
 -- ----------------------------
 -- Records of article
 -- ----------------------------
-INSERT INTO `article` VALUES (1, 1, '测试文章2', 'test111\n\n', 4, 1, 0, 1, 3, 10, 'test111\n\n', 'http://kyou:9000/kkk/119954f26bf246ba99a6cfba6f39b7bc.jpg', '2023-07-26 12:03:47', '1', 'null', '2023-08-17 19:40:00');
-INSERT INTO `article` VALUES (2, 1, '测试文章1', '<p data-v-md-line=\"1\">test</p>\n', 5, 1, 0, 1, 3, 10, 'test\n\n', '', '2023-07-26 13:16:46', '1', 'null', '2023-08-17 19:40:00');
-INSERT INTO `article` VALUES (3, 1, '留言板', '<p data-v-md-line=\"1\">一些优秀的留言</p>\n', 6, 6, 3, 1, 1, 10, '一些优秀的留言', NULL, '2023-07-27 19:09:52', '1', 'null', '2023-08-17 19:40:00');
-INSERT INTO `article` VALUES (4, 3, '测试博客', 'cetset1', 3, 0, 1, 1, 3, 10, 'cetset1', '', '2023-07-31 21:22:28', '3', 'null', '2023-08-17 19:40:00');
-INSERT INTO `article` VALUES (5, 3, '测试文章啊', '啊倒萨', 0, 0, 0, 1, 0, 10, '啊倒萨', 'http://kyou:9000/kkk/b7ec79e388de4757a6e6a6c061fe771a.jpg', '2023-07-31 21:31:29', '3', '1', '2023-07-31 22:10:07');
-INSERT INTO `article` VALUES (6, 1, '又是一篇测试文章', '刷数据', 1, 0, 1, 1, 3, 10, '刷数据', '', '2023-08-03 15:34:01', '1', 'null', '2023-08-17 19:40:00');
+INSERT INTO `article` VALUES (1, 1, '测试文章2', 'test111\n\n', 4, 1, 0, 1, 3, 10, 'test111\n\n', 'http://kyou:9000/kkk/119954f26bf246ba99a6cfba6f39b7bc.jpg', '2023-07-26 12:03:47', '1', 'null', '2023-08-29 10:00:00');
+INSERT INTO `article` VALUES (2, 1, '测试文章1', '<p data-v-md-line=\"1\">test</p>\n', 5, 1, 0, 1, 3, 10, 'test\n\n', '', '2023-07-26 13:16:46', '1', 'null', '2023-08-29 10:00:00');
+INSERT INTO `article` VALUES (3, 1, '留言板', '<p data-v-md-line=\"1\">一些优秀的留言</p>\n', 6, 7, 3, 1, 1, 10, '一些优秀的留言', NULL, '2023-07-27 19:09:52', '1', 'null', '2023-08-29 10:00:00');
+INSERT INTO `article` VALUES (4, 3, '测试博客', 'cetset1', 4, 0, 1, 1, 3, 10, 'cetset1', '', '2023-07-31 21:22:28', '3', 'null', '2023-08-29 10:00:00');
+INSERT INTO `article` VALUES (5, 3, '测试文章啊', '啊倒萨', 0, 0, 0, 1, 3, 10, '啊倒萨', 'http://kyou:9000/kkk/b7ec79e388de4757a6e6a6c061fe771a.jpg', '2023-07-31 21:31:29', '3', '1', '2023-07-31 22:10:07');
+INSERT INTO `article` VALUES (6, 1, '又是一篇测试文章', '刷数据', 1, 1, 1, 1, 3, 10, '刷数据', '', '2023-08-03 15:34:01', '1', 'null', '2023-08-29 10:00:00');
 
 -- ----------------------------
 -- Table structure for article_category
@@ -145,7 +145,7 @@ CREATE TABLE `comments`  (
   `create_time` datetime(0) NULL DEFAULT NULL COMMENT '评论时间',
   `likes` bigint(0) NULL DEFAULT 0 COMMENT '评论点赞数',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '评论表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 13 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '评论表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of comments
@@ -158,6 +158,8 @@ INSERT INTO `comments` VALUES (5, 3, 'kyou', 3, 1, 'kyou', 'http://kyou:9000/kkk
 INSERT INTO `comments` VALUES (8, 1, 'kyou', 3, 3, NULL, 'https://static.juzicon.com/avatars/avatar-200602130320-HMR2.jpeg?x-oss-process=image/resize,w_100', '回复 <span style=\"color: var(--u-color-success-dark-2);\">@kyou:</span> 222', 'Chromium', '0:0:0:0:0:0:0:1', '2023-07-31 20:37:20', 0);
 INSERT INTO `comments` VALUES (9, NULL, NULL, 1, 3, '我的新名字', 'https://static.juzicon.com/avatars/avatar-200602130320-HMR2.jpeg?x-oss-process=image/resize,w_100', '666', 'Chromium', '0:0:0:0:0:0:0:1', '2023-07-31 22:38:59', 0);
 INSERT INTO `comments` VALUES (10, NULL, NULL, 2, 1, 'kyou', 'http://kyou:9000/kkk/119954f26bf246ba99a6cfba6f39b7bc.jpg', '6', 'Chromium', '0:0:0:0:0:0:0:1', '2023-08-02 15:32:29', 0);
+INSERT INTO `comments` VALUES (11, NULL, NULL, 3, 1, 'kyou', 'http://kyou:9000/kkk/119954f26bf246ba99a6cfba6f39b7bc.jpg', 'hash测试', 'Google Chrome', '127.0.0.1', '2023-08-18 14:36:29', 0);
+INSERT INTO `comments` VALUES (12, NULL, NULL, 6, 1, 'kyou', 'http://kyou:9000/kkk/119954f26bf246ba99a6cfba6f39b7bc.jpg', '[大笑2]', 'Google Chrome', '127.0.0.1', '2023-08-18 15:09:53', 0);
 
 -- ----------------------------
 -- Table structure for links
@@ -450,6 +452,11 @@ INSERT INTO `sys_log` VALUES (1687005378849071106, 'admin', '/sys/article/addArt
 INSERT INTO `sys_log` VALUES (1687005378849071107, 'admin', '/sys/article/valid/6', 'UPDATE', '0:0:0:0:0:0:0:1', 0, 'Result com.kyou.blog.background.web.ArticleController.validArticle(Long)', '[6]', '{\"data\":\"\",\"msg\":\"成功\",\"code\":200}', '文章模块', '2023-08-03 15:34:09');
 INSERT INTO `sys_log` VALUES (1688464996683022338, 'admin', '/sys/tag', 'INSERT', '0:0:0:0:0:0:0:1', 0, 'Result com.kyou.blog.background.web.TagController.addTag(String)', '[security]', '{\"data\":\"\",\"msg\":\"成功\",\"code\":200}', '标签模块', '2023-08-07 16:17:12');
 INSERT INTO `sys_log` VALUES (1689443947572199426, 'admin', '/sys/menu/assignPerms/6', 'UPDATE', '127.0.0.1', 0, 'Result com.kyou.blog.background.web.MenuController.assign(Long,List)', '[6, [1, 5, 21, 22, 23, 24, 6, 20, 14]]', '{\"data\":\"\",\"msg\":\"成功\",\"code\":200}', '菜单模块', '2023-08-10 09:09:57');
+INSERT INTO `sys_log` VALUES (1692426097711955969, 'admin', '/sys/comments', 'INSERT', '127.0.0.1', 0, 'Result com.kyou.blog.background.web.CommentsController.addComment(Map,HttpServletRequest)', '[{parentId=null, pname=null, uid=1, address=Google Chrome, content=hash测试, likes=0, createTime=2023-08-18 14:36:29, articleId=3, user={username=kyou, avatar=http://kyou:9000/kkk/119954f26bf246ba99a6cfba6f39b7bc.jpg, level=6, homeLink=/1}}, SecurityContextHolderAwareRequestWrapper[ org.springframework.security.web.header.HeaderWriterFilter$HeaderWriterRequest@710908b1]]', '{\"data\":\"\",\"msg\":\"成功\",\"code\":200}', '评论模块', '2023-08-18 14:36:30');
+INSERT INTO `sys_log` VALUES (1692433647132000257, 'admin', '/sys/comments', 'INSERT', '127.0.0.1', 0, 'Result com.kyou.blog.background.web.CommentsController.addComment(Map,HttpServletRequest)', '[{parentId=null, pname=null, uid=1, address=Google Chrome, content=[大笑2], likes=0, createTime=2023-08-18 15:09:53, articleId=6, user={username=kyou, avatar=http://kyou:9000/kkk/119954f26bf246ba99a6cfba6f39b7bc.jpg, level=6, homeLink=/1}}, SecurityContextHolderAwareRequestWrapper[ org.springframework.security.web.header.HeaderWriterFilter$HeaderWriterRequest@287b3eba]]', '{\"data\":\"\",\"msg\":\"成功\",\"code\":200}', '评论模块', '2023-08-18 15:09:54');
+INSERT INTO `sys_log` VALUES (1696334965689024514, 'admin', '/sys/user/editStatus/1/1/0', 'UPDATE', '127.0.0.1', 0, 'Result com.kyou.blog.background.web.UserController.editUserStatus(Long,Integer,String)', '[1, 1, 0]', '{\"data\":\"\",\"msg\":\"成功\",\"code\":200}', '用户模块', '2023-08-29 09:27:20');
+INSERT INTO `sys_log` VALUES (1696334965689024515, 'admin', '/sys/user/editStatus/1/1/0', 'UPDATE', '127.0.0.1', 0, 'Result com.kyou.blog.background.web.UserController.editUserStatus(Long,Integer,String)', '[1, 1, 0]', '{\"data\":\"\",\"msg\":\"成功\",\"code\":200}', '用户模块', '2023-08-29 09:27:31');
+INSERT INTO `sys_log` VALUES (1696334965689024516, 'admin', '/sys/user/editStatus/1/1/0', 'UPDATE', '127.0.0.1', 0, 'Result com.kyou.blog.background.web.UserController.editUserStatus(Long,Integer,String)', '[1, 1, 0]', '{\"data\":\"\",\"msg\":\"成功\",\"code\":200}', '用户模块', '2023-08-29 09:27:39');
 
 -- ----------------------------
 -- Table structure for tag
@@ -502,9 +509,9 @@ CREATE TABLE `user`  (
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (1, 'admin', '$2a$10$RoIQHqASHRkV1yL4L3t2teDqtvaF77oD3yy8iU9.G1EL3nvDugXWK', '2967861744@qq.com', '1', 'kyou', 'http://kyou:9000/kkk/119954f26bf246ba99a6cfba6f39b7bc.jpg', '超级管理员', '0', 0, '0:0:0:0:0:0:0:1', '2023-07-14 09:51:07', '0', '1', '2023-08-07 17:45:29');
-INSERT INTO `user` VALUES (2, 'commonUser', '$2a$10$dBoIVVOjY4TqqWKJr0bmoOoH0oo/TJNojqO4ExJQC94dGGFFipdsi', '2485119372@qq.com', '1', NULL, 'http://kyou:9000/kkk/8f471cdf6df64c9f969e9e6b19b7ed27.jpg', '一名普通的用户', '0', 0, '0:0:0:0:0:0:0:1', '2023-07-20 09:01:08', '1', '2', '2023-08-01 22:17:09');
-INSERT INTO `user` VALUES (3, 'test1', '$2a$10$NeVEzQAJQZIThgxn7LmXpOOX0zmHfDOwtOvftlOPt5CwzjbvvXyhi', 'asx123@123.com', '1', '我的新名字', 'https://static.juzicon.com/avatars/avatar-200602130320-HMR2.jpeg?x-oss-process=image/resize,w_100', '只是一个普普通通的测试用户罢了', '0', 0, '0:0:0:0:0:0:0:1', '2023-07-20 09:20:05', '1', '3', '2023-08-01 08:58:19');
+INSERT INTO `user` VALUES (1, 'admin', '$2a$10$NgSyJX8.FI3qJAo/XA8o5OMubElmfznaQHQdKp6etODBhArZjlbeS', '2967861744@qq.com', '1', 'kyou', 'http://kyou:9000/kkk/119954f26bf246ba99a6cfba6f39b7bc.jpg', '超级管理员', '0', 0, '0:0:0:0:0:0:0:1', '2023-07-14 09:51:07', '0', '1', '2023-08-29 09:32:27');
+INSERT INTO `user` VALUES (2, 'commonUser', '$2a$10$dBoIVVOjY4TqqWKJr0bmoOoH0oo/TJNojqO4ExJQC94dGGFFipdsi', '2485119372@qq.com', '1', NULL, 'http://kyou:9000/kkk/8f471cdf6df64c9f969e9e6b19b7ed27.jpg', '一名普通的用户', '0', 0, '0:0:0:0:0:0:0:1', '2023-07-20 09:01:08', '1', '1', '2023-08-29 09:43:46');
+INSERT INTO `user` VALUES (3, 'test1', '$2a$10$NeVEzQAJQZIThgxn7LmXpOOX0zmHfDOwtOvftlOPt5CwzjbvvXyhi', 'asx123@123.com', '1', '我的新名字', 'https://static.juzicon.com/avatars/avatar-200602130320-HMR2.jpeg?x-oss-process=image/resize,w_100', '只是一个普普通通的测试用户罢了', '0', 0, '0:0:0:0:0:0:0:1', '2023-07-20 09:20:05', '1', '1', '2023-08-29 09:43:56');
 INSERT INTO `user` VALUES (4, 'test2', '$2a$10$FlwHtD7gPRp8rA9kHiGjveaAYwKooDRPHkuXvEj2aB6OST.C5olUq', 'test2@123.com', '1', '测试用户2', NULL, '测试用户2', '0', 0, '0:0:0:0:0:0:0:1', '2023-07-21 01:11:24', '1', '4', '2023-07-22 09:34:18');
 INSERT INTO `user` VALUES (5, 'test3', '$2a$10$gooI8kcuSgiJLHQZ4s54e.yeo1wGIORASk5WTm89KZHzYR4Uh1i.S', 'test3@123.com', '1', '测试拥护3', NULL, '测试', '0', 0, '0:0:0:0:0:0:0:1', '2023-07-21 01:12:44', '1', '5', '2023-07-22 09:00:27');
 
@@ -523,11 +530,11 @@ CREATE TABLE `user_comment`  (
 -- ----------------------------
 -- Records of user_comment
 -- ----------------------------
-INSERT INTO `user_comment` VALUES (1, 6, 30000, '/userHome1');
-INSERT INTO `user_comment` VALUES (2, 1, 0, '/userHome2');
-INSERT INTO `user_comment` VALUES (3, 1, 330, '/userHome3');
-INSERT INTO `user_comment` VALUES (4, 1, 0, '/userHome4');
-INSERT INTO `user_comment` VALUES (5, 1, 0, '/userHome5');
+INSERT INTO `user_comment` VALUES (1, 6, 30000, '/userHome?id=1');
+INSERT INTO `user_comment` VALUES (2, 1, 0, '/userHome?id=2');
+INSERT INTO `user_comment` VALUES (3, 1, 330, '/userHome?id=3');
+INSERT INTO `user_comment` VALUES (4, 1, 0, '/userHome?id=4');
+INSERT INTO `user_comment` VALUES (5, 1, 0, '/userHome?id=5');
 
 -- ----------------------------
 -- Table structure for user_follow
